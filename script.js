0d3c7009eb6e9cdc32be46894b439d398a4fe4c3
Bituvo <90872694+Bituvo@users.noreply.github.com>
2021-12-10 13:31:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const projects = [
+  new Array('https://bituvo.github.io/wordGenerator', 'Random Word Generator', 'A simple random, pronouceable word generator written in Javascript.', 'Javascript'),
+  new Array('https://google.com/', 'Poopyfard', 'Poopy fard!', 'Poopy!', 'Python'),
+  new Array('https://mail.google.com/', 'I can\'t believe it', 'I am devastated.', 'Javascript'),
+  new Array('https://turbowarp.org/', 'Haha', 'Just kidding! XD', 'Python')
+];
+  
+function addProject(data) {
+  const link = data[0];
+  const title = data[1];
+  const info = data.slice(2, data.length - 1);
+  const language = data[data.length - 1];
+  
+  const projectsDiv = document.getElementById('projects');
+  const newProjectDiv = projectsDiv.createElement('div');
+  newProjectDiv.setAttribute('class', 'project')
+  newProjectDiv.onclick = function {
+    document.location.href = link;
+  };
+  
+  const titleText = newProjectDiv.createElement('h1');
+  titleText.innerText = title;
+};
 
+addProject(projects[3]);
